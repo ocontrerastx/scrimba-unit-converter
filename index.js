20a73fd6c9e40de68cc9f64c698e47010066fe70
Osvaldo Contreras <ocontreras.tx@gmail.com>
2025-05-02 08:56:19 -0500

    Adding IDs to select elements in Javascript and setting up Javascript file.

--- index.js
+++ index.js
@@ -3,3 +3,9 @@
 1 liter = 0.264 gallon
 1 kilogram = 2.204 pound
 */
+
+const lengthEl = document.getElementById("length-el")
+const volumeEl = document.getElementById("volume-el")
+const massEl = document.getElementById("mass-el")
+
+console.log(massEl)
